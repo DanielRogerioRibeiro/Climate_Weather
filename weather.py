e316b4945d9a3a4d68c03acace79c36cdd23f67a
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-30 21:47:36 -0300

    Creating the time window

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ from PyQt5 import  uic,QtWidgets
 
 app=QtWidgets.QApplication([])
 main=uic.loadUi("main.ui")
-main.pushButton.clicked.connect(funcao_principal)
+#main.pushButton.clicked.connect(funcao_principal)
 #formulario.pushButton_2.clicked.connect(chama_segunda_tela)
 
 main.show()
