16d6a86da6a6393d2d8e41b29bd12818599a7926
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-06-01 21:47:08 -0300

    Creating the info function

--- weather.py
+++ weather.py
@@ -43,6 +43,30 @@ frame_quadros.grid(row=2, column=0, sticky=NW)
 style = ttk.Style(frame_principal)
 style.theme_use("clam")
 
+
+def info():
+    #weather_key = '74db8d03761c1e007553ccbc6d73fe92'
+    #cidade = e_local.get()
+    #api_link = "https://api.openweathermap.org/data/2.5/weather?q="+cidade+"&appid="+ weather_key+"&lang=pt"
+
+    #HTTP request
+    r=requests.get(api_link)
+    #convert the data in 'r' into dictionary
+    data=r.json()
+
+    # zona , pais, horas 
+    pais_codigo = data['sys']['country']
+
+    zona_fuso=pytz.country_timezones[pais_codigo]
+
+    # --- pais ---
+    pais = pytz.country_names[pais_codigo]
+
+    # --- data ---
+    zona = pytz.timezone(zona_fuso[0]) 
+    zona_horas = datetime.now(zona)
+    zona_horas = zona_horas.strftime("%d %m %Y | %H:%M:%S %p")
+
 app=QtWidgets.QApplication([])
 main=uic.loadUi("main.ui")
 #main.pushButton.clicked.connect(funcao_principal)
